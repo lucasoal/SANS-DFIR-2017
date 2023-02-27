440c44017f5f2e2e8fd6c9da0f26e4853f298985
lucasoal <lucasoalzst@gmail.com>
2023-02-26 23:47:12 -0300

    refatoracao utilizando pep8

--- bitcoinmapping.py
+++ bitcoinmapping.py
@@ -1,51 +1,51 @@
 import json
+
 import requests
 
 z = 0
 i = 0
-firstpart = "https://blockchain.info/rawaddr/"
-initialinput = input("please type the 'seed' address: ")
-initialreq = firstpart + initialinput
+first_part = "https://blockchain.info/rawaddr/"
+initial_input = input("please type the 'seed' address: ")
+initial_req = first_part + initial_input
 
-firstjson = (requests.get(initialreq)).json()
-graphvizlines = []
+first_json = (requests.get(initial_req)).json()
+graphviz_lines = []
 
-addresslist = []
-usedaddresslist = []
+address_list = []
+used_address_list = []
 
-addresslist.append(initialinput)
-usedaddresslist.append(initialinput)
+address_list.append(initial_input)
+used_address_list.append(initial_input)
 
 while i < 6:
-    if z is 1:
-        initialreq = firstpart + addresslist[i]
-        firstjson = (requests.get(initialreq)).json()
-    
-    for transaction in firstjson["txs"]:
-        payerlist = []
-        recipientlist = []
-        
+    if z == 1:
+        initial_req = first_part + address_list[i]
+        first_json = (requests.get(initial_req)).json()
+
+    for transaction in first_json["txs"]:
+        payer_list = []
+        recipient_list = []
+
         print("\n" + transaction["hash"])
 
         for item in transaction["inputs"]:
-            payerlist.append(item["prev_out"]["addr"])
-            if item["prev_out"]["addr"] not in addresslist:
-                addresslist.append(item["prev_out"]["addr"])
+            payer_list.append(item["prev_out"]["addr"])
+            if item["prev_out"]["addr"] not in address_list:
+                address_list.append(item["prev_out"]["addr"])
 
         for target in transaction["out"]:
-            recipientlist.append(target["addr"])
-            if target["addr"] not in addresslist:
-                addresslist.append(target["addr"])
+            recipient_list.append(target["addr"])
+            if target["addr"] not in address_list:
+                address_list.append(target["addr"])
 
-        for payer in payerlist:
-            for recipient in recipientlist:
+        for payer in payer_list:
+            for recipient in recipient_list:
                 a = '"' + payer + '"' + " -> " + '"' + recipient + '"' + ";"
-                if a not in graphvizlines:
-                    graphvizlines.append(a)
-    i = i + 1    
+                if a not in graphviz_lines:
+                    graphviz_lines.append(a)
+    i += 1
     z = 1
-        
 
-for t in graphvizlines:
+
+for t in graphviz_lines:
     print(t)
-
